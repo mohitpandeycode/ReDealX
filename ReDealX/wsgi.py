20008abe522f34d25a048a0215d5e354b8ad17cc
Mohit Pandey <mp7698921@gmail.com>
2025-02-16 21:43:02 +0530

    fix bugs

--- ReDealX/wsgi.py
+++ ReDealX/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unnamed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReDealX.settings')
 
 application = get_wsgi_application()
