80c586d7ce5bdcf4aab0e2d834da5dec379f2df8
Mohit Pandey <mp7698921@gmail.com>
2025-08-04 02:57:19 +0530

    add chaching

--- home/views.py
+++ home/views.py
@@ -14,9 +14,11 @@ import random
 from django.core.paginator import Paginator
 from django.db.models.functions import Random
 from django.db.models import Prefetch, F
+from django.views.decorators.cache import cache_page
 
 
 # Home page
+@cache_page(60 * 5)
 def index(request):
     # Handle login/signup early
     auth_response = handle_user_auth(request)
@@ -115,6 +117,7 @@ def handle_logout(request):
 
 
 # All products page
+@cache_page(60 * 10)
 def allproducts(request):
     # Handle authentication (POST)
     auth_response = handle_user_auth(request)
@@ -260,6 +263,7 @@ def search_products(request):
 
 
 # Products by category
+@cache_page(60 * 10)
 def prodbyCategory(request, category):
      # Check for signup/login handling
     auth_response = handle_user_auth(request)
@@ -528,6 +532,7 @@ def viewSeller(request, slug):
     return render(request, 'sellerprofile.html', context)
 
 # Wishlist
+@cache_page(60 * 10)
 @login_required
 def wishlist(request):
     # Handle search queries early
